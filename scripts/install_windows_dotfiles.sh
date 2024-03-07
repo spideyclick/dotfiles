5fa082c203b1a04eb1cdf2527adad8ca3ca5fcb0
zhubbell <zhubbell@midwestholding.com>
2024-03-07 09:43:50 -0600

    2024-03-07

---
+++ scripts/install_windows_dotfiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+cp ../config/windows_terminal/settings.json /mnt/c/Users/zhubbell/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+cp ../config/wezterm/wezterm.lua /mnt/c/Users/zhubbell/
+
