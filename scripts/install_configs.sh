098453969a510b28cfdb319d60cd7938e141c4d0
zhubbell <zhubbell@midwestholding.com>
2024-04-10 11:43:37 -0500

    Further absolute links

--- scripts/install_configs.sh
+++ scripts/install_configs.sh
@@ -5,11 +5,12 @@ cd "$(dirname $0)/../config" || exit
 mkdir -p ~/.config
 user_home_dir=$(getent passwd "$(whoami)" | cut -d: -f6)
 config_dir="${user_home_dir}/.config"
+dotfiles_config_dir="${user_home_dir}/dotfiles/config"
 
-ln -s .zshrc "${user_home_dir}/.zshrc"
-ln -s starship.toml "${config_dir}/starship.toml"
-ln -s helix "${config_dir}/helix"
-ln -s joshuto "${config_dir}/joshuto"
-ln -s lazygit "${config_dir}/lazygit"
-ln -s ranger "${config_dir}/ranger"
+ln -s "${dotfiles_config_dir}/.zshrc" "${user_home_dir}/.zshrc"
+ln -s "${dotfiles_config_dir}/starship.toml" "${config_dir}/starship.toml"
+ln -s "${dotfiles_config_dir}/helix" "${config_dir}/helix"
+ln -s "${dotfiles_config_dir}/joshuto" "${config_dir}/joshuto"
+ln -s "${dotfiles_config_dir}/lazygit" "${config_dir}/lazygit"
+ln -s "${dotfiles_config_dir}/ranger" "${config_dir}/ranger"
 
