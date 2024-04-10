8ecab672c290b269b53d3ba12a9e56c2e20bed4e
zhubbell <zhubbell@midwestholding.com>
2024-04-10 11:20:56 -0500

    Fix config installation

--- scripts/install_configs.sh
+++ scripts/install_configs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.config
 cd "$(dirname $0)/../config" || exit
 
 ln -s .zshrc ~/.zshrc
-ln -s .helix ~/.config/helix
-ln -s .joshuto ~/.config/joshuto
-ln -s .lazygit ~/.config/lazygit
-ln -s .ranger ~/.config/ranger
+ln -s starship.toml ~/.config/starship.toml
+ln -s helix ~/.config/helix
+ln -s joshuto ~/.config/joshuto
+ln -s lazygit ~/.config/lazygit
+ln -s ranger ~/.config/ranger
 
