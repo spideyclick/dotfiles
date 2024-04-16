c561489e36ea0c7c8b7e8098b62b6eae583b6f82
zhubbell <zhubbell@midwestholding.com>
2024-04-16 13:09:17 -0500

    Improve helix langs, skip brew installation if not needed

--- install.sh
+++ install.sh
@@ -6,20 +6,22 @@ cd "$(dirname $0)" || exit
 ### Detect OS
 echo "$PREFIX" | grep termux
 if [ $? -eq 0 ]; then
-  echo "Termux detected"
-  ./setup_termux.sh
-  exit 0
+	echo "Termux detected"
+	./setup_termux.sh
+	exit 0
 fi
 OS_ID=$(grep -ioP '^ID=\K\S+$' /etc/os-release)
 echo "Current OS appears to be: ${OS_ID}"
 if [[ ${OS_ID} == "debian" || ${OS_ID} == "ubuntu" ]]; then
-  echo "This OS should be able to work with the homebrew strategy"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.bashrc
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  brew bundle install ./Brewfile
-  ./scripts/install_local_bin.sh
-  ./scripts/install_configs.sh
+	echo "This OS should be able to work with the homebrew strategy"
+	if ! command -v brew &> /dev/null; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.bashrc
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	fi
+	brew bundle install ./Brewfile
+	./scripts/install_local_bin.sh
+	./scripts/install_configs.sh
 else
-  echo "No handler for current OS found - exiting"
+	echo "No handler for current OS found - exiting"
 fi
