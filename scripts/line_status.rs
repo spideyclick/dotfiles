57d412231c0a5b19c305f5e7eb484d01345a45b4
zhubbell <zhubbell@midwestholding.com>
2024-12-18 14:08:15 -0600

    Add line_status

---
+++ scripts/line_status.rs
@@ -0,0 +1,39 @@
+#!/usr/bin/env rust-script
+
+//! This file requires cargo script to be installed: cargo install cargo-script
+//! This is a regular crate doc comment, but it also contains a partial
+//! Cargo manifest.  Note the use of a *fenced* code block, and the
+//! `cargo` "language".
+//!
+//! ```cargo
+//! [dependencies]
+//! regex = "1.6.0"
+//! ```
+
+extern crate regex;
+
+use regex::Regex;
+use std::io::{stdin, IsTerminal};
+use std::env::args;
+
+fn main() {
+	// match IsTerminal() {}
+	let input = stdin();
+	match input.is_terminal() {
+		true => {
+			println!("No input detected from STDIN")
+		}
+		false => {
+			let icon = args().nth(1).unwrap_or('📃'.to_string());
+			let mut input_string = String::new();
+			input
+				.read_line(&mut input_string)
+				.expect("Could not read from STDIN!");
+			let trimmed_string = input_string.trim_end();
+			let regex = Regex::new(r"^(\s+)?(- )?(\[[ xX\*\|]\]|✅|⛔|📃|🚧|🟡)?(.*$)").unwrap();
+			let substitution = format!("$1$2{}$4", icon);
+			let result = regex.replace_all(trimmed_string, substitution);
+			println!("{}", result);
+		}
+	}
+}
