89326cd754f9286d6c4b81fc3e2928b305976e8c
spideyclick <spideyclick@gmail.com>
2025-02-19 22:39:34 -0600

    .

--- scripts/setup_termux.sh
+++ scripts/setup_termux.sh
@@ -1,5 +1,12 @@
 yes | pkg upgrade
-pkg install zsh
+
+git clone https://github.com/notflawffles/termux-nerd-installer.git ~
+cd ~/termux-nerd-installer
+make install
+termux-nerd-installer install jetbrains-mono-ligatures
+termux-nerd-installer set jetbrains-mono-ligatures
+cd -
+
 pkg install helix
 pkg install ranger
 pkg install curl
@@ -13,5 +20,3 @@ pkg install zellij
 cargo install bacon
 python -m pip install --upgrade pip
 python -m pip install pdb
-
-chsh -s $(which zsh)
