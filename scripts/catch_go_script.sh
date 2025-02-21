a3f5dc42a6bccd4629e1d30aaac1f42d61f1200b
spideyclick <spideyclick@gmail.com>
2025-02-21 09:03:43 -0600

    .

--- scripts/catch_go_script.sh
+++ scripts/catch_go_script.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+./install.sh
 
-### Detect OS
-echo $PREFIX | grep termux
-if [ $? -eq 0 ]; then
-  echo "Termux detected"
-  ./setup_termux.sh
-  exit 0
-fi
-OS_ID=$(grep -ioP '^ID=\K\S+$' /etc/os-release)
-echo "Current OS appears to be: ${OS_ID}"
-if [[ ${OS_ID} == "debian" ]]; then
-  echo "Debian detected"
-  ./install_nix_on_debian.sh
-else
-  echo "OS unknown: ${OS_ID}"
-fi
-
-./install_dotfiles.sh
