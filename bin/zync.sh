28dc046f3130a4dd1231cb6bb5d828b6575d3ec6
spideyclick <spideyclick@gmail.com>
2025-05-02 17:12:44 -0500

    .

--- bin/zync.sh
+++ bin/zync.sh
@@ -9,38 +9,45 @@
 ###############################################################################
 # Parameters                                                                  #
 ###############################################################################
-# Don't use trailing slashes in directory paths!
-TARGET=$1
+TARGET=${ZYNC_TARGET:-$1}
+TARGET=${TARGET%/}
 
-echo "Starting initial Sync..."
+echo "Checking initial Sync..."
+rsync \
+  --exclude '.envrc' \
+  --exclude '.git' \
+  --delete \
+  -arvz . "$TARGET/" \
+  --dry-run \
+  | tspin --highlight red:deleting;
 
+read -p "Press enter to continue"
 rsync \
   --progress \
-  --relative \
+  --exclude '.envrc' \
   --exclude '.git' \
-  -vraze \
-  'ssh -p 22' . "${TARGET}/" \
-  ;
+  --delete \
+  -arvz . "$TARGET/" \
+  | tspin --highlight red:deleting;
 
-###############################################################################
-# Watch for changes & sync                                                    #
-###############################################################################
-inotifywait -mr \
-  --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
-  --exclude '\.git' \
-  -e close_write . |
-while read -r date time dir file; do
-  CHANGED_ABS="${dir}${file}"
-  CHANGED_REL="${CHANGED_ABS#"$CURRENT_DIRECTORY"/}"
-  echo "$CHANGED_ABS"
-  echo "$CHANGED_REL"
-  rsync \
-    --progress \
-    --relative \
-    -vraze \
-    'ssh -p 22' \
-    "${CHANGED_REL}" \
-    "${TARGET}/" && \
-      echo "At ${time} on ${date}, file $CHANGED_ABS was backed up via rsync" >&2
-done
+# ###############################################################################
+# # Watch for changes & sync                                                    #
+# ###############################################################################
+# inotifywait -mr \
+#   --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
+#   --exclude '\.git' \
+#   -e close_write . |
+# while read -r date time dir file; do
+#   CHANGED_ABS="${dir}${file}"
+#   CHANGED_REL="${CHANGED_ABS#"$CURRENT_DIRECTORY"/}"
+#   echo "$CHANGED_ABS"
+#   echo "$CHANGED_REL"
+#   rsync \
+#     --progress \
+#     --relative \
+#     -arvz \
+#     "${CHANGED_REL}" \
+#     "${TARGET}/" && \
+#       echo "At ${time} on ${date}, file $CHANGED_ABS was backed up via rsync" >&2
+# done
 
